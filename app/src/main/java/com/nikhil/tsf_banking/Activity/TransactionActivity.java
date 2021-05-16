d840b0ecb98e90c66e431627ee635bb8c58cbc0a
KOTHA NIKHIL REDDY <nikhilreddykotha98@gmail.com>
2021-05-16 17:19:34 +0530

    banking app

---
+++ app/src/main/java/com/nikhil/tsf_banking/Activity/TransactionActivity.java
@@ -0,0 +1,48 @@
+package com.nikhil.tsf_banking.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import com.nikhil.tsf_banking.Adapter.TransacAdapter;
+import com.nikhil.tsf_banking.R;
+import com.nikhil.tsf_banking.db.TransacDatabase;
+import com.nikhil.tsf_banking.db.TransacEntity;
+
+import java.util.List;
+
+public class TransactionActivity extends AppCompatActivity {
+
+    RecyclerView rec;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+
+        rec = findViewById(R.id.transRec);
+        try {
+            List<TransacEntity> list = new getList().execute(TransactionActivity.this).get();
+            TransacAdapter adapter = new TransacAdapter(list);
+            rec.setAdapter(adapter);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(TransactionActivity.this);
+            rec.setLayoutManager(layoutManager);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class getList extends AsyncTask<Context, Void, List<TransacEntity>> {
+
+        @Override
+        protected List<TransacEntity> doInBackground(Context... contexts) {
+            TransacDatabase db = Room.databaseBuilder(contexts[0], TransacDatabase.class, "transaction").build();
+            return db.getDao().getAll();
+        }
+    }
+}
